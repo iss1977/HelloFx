72eb4a5609988451a390a8bc244710d689f88d11
Ioan-Sebastian Szücs <ioan.szucs@gmail.com>
2020-08-05 15:26:01 +0200

    Ende Tag Codersbay

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,51 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) {
-        Application.launch(MyFirstApp.class, args);
+    @Override
+    public void init() throws Exception {
+        System.out.println("Start");
+        super.init();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        stage.setWidth(500);
+        stage.setHeight(400);
+
+        VBox parent = new VBox();
+        Label label1 = new Label("This is a label");
+
+        parent.getChildren().add(label1);
+
+        Scene scene1 =  new Scene(parent);
+        stage.setScene(scene1);
+        
+
+
+        stage.show();
+
+
+
+
+    }
+
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Ende");
+        super.stop();
+    }
+
+    public static void main  (String[] args) {
+
+        launch();
 
     }
 }
